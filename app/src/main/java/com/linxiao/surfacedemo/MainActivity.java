5738f1151e74078e8b48d89080999c62d38565da
Mr.Li <lizheng@openfin.com>
2018-08-20 16:38:12 +0800

    touch demo

--- app/src/main/java/com/linxiao/surfacedemo/MainActivity.java
+++ app/src/main/java/com/linxiao/surfacedemo/MainActivity.java
@@ -1,9 +1,13 @@
 package com.linxiao.surfacedemo;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+  private SurfaceView mSurfaceView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/linxiao/surfacedemo/SurfaceViewSinFun.java
+++ app/src/main/java/com/linxiao/surfacedemo/SurfaceViewSinFun.java
@@ -5,9 +5,17 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.nfc.Tag;
+import android.os.SystemClock;
+import android.support.v4.view.InputDeviceCompat;
+import android.support.v4.view.MotionEventCompat;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import java.util.Locale;
 
 /**
  * Description
@@ -15,6 +23,7 @@ import android.view.SurfaceView;
  * Create Data  2018\8\2 0002
  */
 public class SurfaceViewSinFun extends SurfaceView implements SurfaceHolder.Callback, Runnable {
+  private static final String TAG = "touch_event";
   private SurfaceHolder mSurfaceHolder;
   //绘图的Canvas
   private Canvas mCanvas;
@@ -24,6 +33,7 @@ public class SurfaceViewSinFun extends SurfaceView implements SurfaceHolder.Call
   private Paint mPaint;
   private Path mPath;
   private Paint mTempPaint;
+  private StringBuilder stringBuilder;
 
   public SurfaceViewSinFun(Context context) {
     this(context, null);
@@ -72,13 +82,13 @@ public class SurfaceViewSinFun extends SurfaceView implements SurfaceHolder.Call
 
   @Override
   public void run() {
-    while (mIsDrawing) {
-      drawSomething();
-      x += 1;
-      y = (int) (100 * Math.sin(2 * x * Math.PI / 180) + 400);
-      //加入新的坐标点
-      mPath.lineTo(x, y);
-    }
+//    while (mIsDrawing) {
+//      drawSomething();
+//      x += 1;
+//      y = (int) (100 * Math.sin(2 * x * Math.PI / 180) + 400);
+//      //加入新的坐标点
+//      mPath.lineTo(x, y);
+//    }
   }
 
   int num = 0;
@@ -90,7 +100,7 @@ public class SurfaceViewSinFun extends SurfaceView implements SurfaceHolder.Call
       //通过下面的测试，证明canvas中是保留上次绘制的内容的
       if (num > 100 & num < 300) {
         mCanvas.drawCircle(200, 200, 100, mTempPaint);
-      }else{
+      } else {
         //绘制背景
         mCanvas.drawColor(Color.RED);
       }
@@ -116,6 +126,54 @@ public class SurfaceViewSinFun extends SurfaceView implements SurfaceHolder.Call
     setFocusable(true);
     setKeepScreenOn(true);
     setFocusableInTouchMode(true);
+  }
+
+  @Override
+  public boolean onTouchEvent(MotionEvent event) {
+    StringBuilder stringBuilder = new StringBuilder();
+    int actionMasked = event.getActionMasked();
+    stringBuilder.append("action code: ")
+        .append(actionMasked)
+        .append("  ");
+    switch (actionMasked) {
+      case MotionEvent.ACTION_DOWN: {
+        stringBuilder.append("单点按下");
+        break;
+      }
+      case MotionEvent.ACTION_UP: {
+        stringBuilder.append("单点抬起");
+        break;
+      }
+      case MotionEvent.ACTION_MOVE: {
+        stringBuilder.append("单点移动");
+        break;
+      }
+      case MotionEvent.ACTION_CANCEL: {
+        stringBuilder.append("触摸动作取消");
+        break;
+      }
+      case MotionEvent.ACTION_OUTSIDE: {
+        stringBuilder.append("触摸动作超出边界");
+        break;
+      }
+      case MotionEvent.ACTION_POINTER_DOWN: {
+        stringBuilder.append("多点按下");
+        break;
+      }
+      case MotionEvent.ACTION_POINTER_UP: {
+        stringBuilder.append("多点抬起");
+        break;
+      }
+    }
+    stringBuilder.append("  ");
+    for (int i = 0; i < event.getPointerCount(); i++) {
+      float x = event.getX(i);
+      float y = event.getY(i);
+      stringBuilder.append(String.format(Locale.getDefault(), "%d：(%f,%f)", i, x, y)).append("  ");
+    }
+
+    Log.d(TAG, stringBuilder.toString());
+    return true;
   }
 }
 
